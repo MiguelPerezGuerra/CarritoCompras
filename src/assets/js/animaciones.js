5d40f3264da86213adbdfc65158046d9addb0a3b
MiguelPerez <midapegue@gmail.com>
2026-03-25 20:32:40 -0500

    Maquetacion y animacion footer y nav: -index.html: modificacion de footer, eliminacion de form main.scss: eliminacion de estilos form, modificacion de social icons -main.scss: agregado de animacion en menu nav

--- src/assets/js/animaciones.js
+++ src/assets/js/animaciones.js
@@ -5,11 +5,11 @@ const bannerContenedor = document.getElementById('banner-contenedor');
 const original = document.getElementsByClassName('original');
 
 function copiarBanner() {
-        for (let i = 0; i < 6; i++) {
-            const clon = original[0].cloneNode(true);
-            clon.classList.remove('original');
-            bannerContenedor.appendChild(clon);
-        }
+    for (let i = 0; i < 6; i++) {
+        const clon = original[0].cloneNode(true);
+        clon.classList.remove('original');
+        bannerContenedor.appendChild(clon);
+    }
 }
 
 export { copiarBanner };
@@ -22,7 +22,33 @@ btnMenu.addEventListener('click', () => {
     const navActivo = document.querySelector('.nav__activo');
     const navClose = document.querySelector('.nav__close');
     const navItems = document.querySelector('#nav-list');
+    const isMenuClosed = navItems.classList.contains('activo');
+
+    if (isMenuClosed) {
+        // Abrir menu: nav__list aparece desde arriba
+        navItems.classList.remove('activo');
+        navItems.classList.add('sliding-in-list');
+
+        navActivo.classList.add('sliding-in-menu')
+
+        setTimeout(() => {
+            navItems.classList.remove('sliding-in-list');
+            navActivo.classList.remove('sliding-in-menu')
+        }, 500);
+    } else {
+        // Cerrar menu: nav__list desaparece hacia arriba
+        navItems.classList.add('sliding-out-list');
+
+        navActivo.classList.add('sliding-out-menu');
+
+        setTimeout(() => {
+            navItems.classList.add('activo');
+            navItems.classList.remove('sliding-out-list');
+
+            navActivo.classList.add('sliding-out-menu');
+        }, 300);
+    }
+
     navActivo.classList.toggle('activo');
     navClose.classList.toggle('activo');
-    navItems.classList.toggle('activo');
 });
